0c46e0d1d9b23e70f5be1e8e7a9852839119aac1
osua <79318129+osua@users.noreply.github.com>
2021-08-18 15:11:17 -0400

    ooo

--- components/layout.js
+++ components/layout.js
@@ -1,14 +1,14 @@
-import Alert from '../components/alert'
-import Footer from '../components/footer'
+//import Alert from '../components/alert'      LINE-8  <Alert preview={preview} />
+
+import Footer from '../components/footer';
 
 export default function Layout({ preview, children }) {
   return (
     <>
       <div className="min-h-screen">
-        <Alert preview={preview} />
         <main>{children}</main>
       </div>
       <Footer />
     </>
-  )
+  );
 }
