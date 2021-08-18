808d7bd66d4ccb2682fe4e167a2cb51dbb69775c
osua <79318129+osua@users.noreply.github.com>
2021-08-18 18:15:49 -0400

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,8 @@
 ////href="https://www.datocms.com/docs/next-js" -------Line 13
 ////className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0" -------Line 12
 ////> -------Line 15
-////            </a> ----- Line 16
+////              Read Documentation -------Line 16
+////</a> -------Line 17
 import Container from './container';
 
 export default function Footer() {
@@ -11,11 +12,9 @@ export default function Footer() {
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Copyright © 2021 ModusNYC by 0 Su
+            Copyright © 2021 ModusNYC -0 Su
           </h3>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            Read Documentation
-          </div>
+          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2"></div>
         </div>
       </Container>
     </footer>
