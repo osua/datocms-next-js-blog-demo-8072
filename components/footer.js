bb06e78c3a30f5b67c04de13fda7d54356ceabe3
osua <79318129+osua@users.noreply.github.com>
2021-08-18 18:11:04 -0400

    aaaaa

--- components/footer.js
+++ components/footer.js
@@ -1,3 +1,8 @@
+////            <a --------------------Line 12
+////href="https://www.datocms.com/docs/next-js" -------Line 13
+////className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0" -------Line 12
+////> -------Line 15
+////            </a> ----- Line 16
 import Container from './container';
 
 export default function Footer() {
@@ -6,15 +11,10 @@ export default function Footer() {
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Copyright © 2021 Powered by Next.js ModusNYC by 0 Su
+            Copyright © 2021 ModusNYC by 0 Su
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            <a
-              href="https://www.datocms.com/docs/next-js"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-            >
-              Read Documentation
-            </a>
+            Read Documentation
           </div>
         </div>
       </Container>
--- components/intro.js
+++ components/intro.js
@@ -1,3 +1,9 @@
+///        <a         ----------------------Line 9
+///          href="https://nextjs.org/" -----Line 10
+///          className="underline hover:text-success duration-200 transition-colors" -----Line 11
+///        > -----Line 12
+///          Next.js -----Line 13
+///        </a> -----Line 14
 export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
@@ -5,7 +11,7 @@ export default function Intro() {
         Modus NYC.
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        An online Traveler's Toolkit, a Swiss Army Knife for your inner voyager{' '}
+        An online Traveler's Toolkit, a Swiss Army Knife, for your inner voyager{' '}
         .
       </h4>
     </section>
